f1494c925b99384c928f32b0e795e54d948b76fb
ron_zhong <ronzhong2009@gmail.com>
2019-06-01 21:30:19 +0800

    add another drag amd drop api

--- AngularjsDataVisualizations/src/app/app.component.ts
+++ AngularjsDataVisualizations/src/app/app.component.ts
@@ -98,7 +98,59 @@ export class AppComponent {
       this.max = this.data[i].value;
     }
     // End Controller 
+
+
+
+    let dropbox;
+
+    dropbox = document.getElementById("dropbox");
+    dropbox.addEventListener("dragenter", this.dragenter, false);
+    dropbox.addEventListener("dragover", this.dragover, false);
+    dropbox.addEventListener("drop", this.drop, false);
   }
+
+
+
+   dragenter(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+  
+   dragover(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+   drop(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  
+    const dt = e.dataTransfer;
+    const files = dt.files;
+  
+    this.handleFiles(files);
+  }
+
+
+
+  handleFiles(files) {
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      
+      if (!file.type.startsWith('image/')){ continue }
+      
+      const img = document.createElement("img");
+      img.classList.add("obj");
+      img.file = file;
+      // preview.appendChild(img); // Assuming that "preview" is the div output where the content will be displayed.
+      this.dotL.nativeElement.appendChild(img);
+      const reader = new FileReader();
+      reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
+      reader.readAsDataURL(file);
+    }
+  }
+
+
 
   handleMassifData(){
     // console.log("inputString:" + this.inputString);
